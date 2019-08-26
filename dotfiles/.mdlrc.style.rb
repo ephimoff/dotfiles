941db6e450d2784b637530ff726826f0adb62188
Anton Efimov <anton.efimov@auto1.com>
2019-08-26 15:15:56 +0200

    Added vs code extensions and removed markdownlint

--- dotfiles/.mdlrc.style.rb
+++
@@ -1,9 +0,0 @@
-all
-
-exclude_rule 'MD013' # Line length
-exclude_rule 'MD014' # Dollar signs before commands
-exclude_rule 'MD033' # Inline HTML
-exclude_rule 'MD040' # Code blocks should have a language
-
-rule 'MD007', :indent => 2 # Unordered list indentation
-rule 'MD024', :siblings_only => true
