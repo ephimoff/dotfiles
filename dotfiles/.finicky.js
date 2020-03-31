52195206ef01363e745dece1e767bfc0baed5cd4
Anton Efimov <anton.efimov@auto1.com>
2020-03-31 13:13:25 +0200

    Updating finicky with new config

--- dotfiles/.finicky.js
+++ dotfiles/.finicky.js
@@ -4,12 +4,19 @@ module.exports = {
     {
       // Open these urls in Chrome
       match: finicky.matchHostnames([
-        "docs.google.com", 
-        "console.cloud.google.com",
+        "www.smartrecruiters.com",
+        /.*\.google.com$/, // match all google subdomains
         /wkdauto/, 
         /auto1/, 
       ]),
       browser: "Google Chrome"
     },
+    // {
+    //   // Open these urls in Slack app
+    //   match: finicky.matchHostnames([
+    //     "wkda-eng.slack.com",
+    //   ]),
+    //   browser: "com.tinyspeck.slackmacgap"
+    // },
   ]
 };
