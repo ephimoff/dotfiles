0bb28f9268f4460d8d11e135addb5b04d935f96e
Anton Efimov <a.ephimoff@gmail.com>
2021-01-05 16:14:16 +0100

    Configuring CI

--- tests/2_apply_dotfiles.sh
+++ tests/2_apply_dotfiles.sh
@@ -6,4 +6,9 @@ IFS=$'\n'
 
 # run dotfiles
 
-"./bootstrap.sh"
+./install/1-directories.sh
+./install/2-macos.sh
+./install/3-brew.sh
+./install/4-dotfiles.sh
+./install/5-applications.sh
+./install/6-oh-my-zsh.sh
