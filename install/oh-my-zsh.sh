4ee73d1c045ea4ad45a2b20e02a1a6c70a1df299
Anton Efimov <a.ephimoff@gmail.com>
2021-02-11 13:15:04 +0100

    one more cleanup

--- install/oh-my-zsh.sh
+++ install/oh-my-zsh.sh
@@ -5,8 +5,8 @@ echo "Starting $(basename "$0")"
 echo "Installing oh-my-zsh (http://ohmyz.sh)"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "Instaling oh-my-zsh theme: powerlevel9k (see https://github.com/bhilburn/powerlevel9k for more info)"
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+# echo "Instaling oh-my-zsh theme: powerlevel9k (see https://github.com/bhilburn/powerlevel9k for more info)"
+# git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 echo "Instaling zsh-autosuggestions plugin"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
