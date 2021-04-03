62e2979d9b989c8cf1ab4a7fcde14bb222c75411
Anton Efimov <2668603+ephimoff@users.noreply.github.com>
2021-04-03 18:12:21 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,6 @@ echo "Note that some of these changes require a logout/restart to take effect."
 
 
 ./install/brew-install.sh
-# ./install/directories.sh
 ./install/brew.sh
 ./install/dotfiles.sh
 ./install/applications.sh
